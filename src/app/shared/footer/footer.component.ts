2d28e022da112656a9371e3bb6665787c0f9296f
Sergio <sergioaldu11@gmail.com>
2022-06-03 19:12:55 +0200

    Implementación footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-    test : Date = new Date();
 
-    constructor() { }
+  footerItems =[{
+    'icon':'bi bi-house',
+    'class':'col-2',
+    'url':'/'
+  },
+  {
+    'titulo':'About',
+    'class':'col-8',
+    'url':'/about'
+  },
+  {
+    'titulo':'Contact',
+    'class':'contact',
+    'url':'/contact'
+  }];
 
-    ngOnInit() {}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
